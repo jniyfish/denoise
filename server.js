f7e86fefc2a8070a81304b6c5bdc931adb9d9d36
jniyfish <39803423+jniyfish@users.noreply.github.com>
2019-10-22 23:25:45 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,12 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use(express.static("./static"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log(`Server listening on port ${port}!`);
+});
+
 
 app.listen(81, function()
 {
